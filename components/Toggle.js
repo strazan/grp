a03243e33e04ffd29f5c86f618453f1ec3810902
strazan <sigge@labor.cool>
2020-05-15 08:54:22 +0200

    fix: typo in imports

--- components/Toggle.js
+++ components/Toggle.js
@@ -1,31 +1,28 @@
-import { useState, useRef, useEffect } from 'react';
-import style from './style/toggle.module.scss';
+import style from "./Style/toggle.module.scss";
+import { useState, useRef, useEffect } from "react";
 
 const Toggle = (props) => {
-     //  const button = useRef();
-     //  const [toggle, setToggle] = useState(true);
-     const input = useRef();
+  const input = useRef();
 
-     useEffect(() => {
-          input.current.checked = props.isToggled;
-          console.log(props.isToggled);
-     }, []);
+  useEffect(() => {
+    input.current.checked = props.isToggled;
+  }, []);
 
-     return (
-          <div className={style.container}>
-               <label class={style.switch}>
-                    <input
-                         ref={input}
-                         type="checkbox"
-                         onClick={(e) => {
-                              props.click();
-                         }}
-                    />
-                    <span className={style.slider} />
-               </label>
-               <h5>{props.toggleTitle}</h5>
-          </div>
-     );
+  return (
+    <div className={style.container}>
+      <label class={style.switch}>
+        <input
+          ref={input}
+          type="checkbox"
+          onClick={(e) => {
+            props.click();
+          }}
+        />
+        <span className={style.slider} />
+      </label>
+      <h5>{props.toggleTitle}</h5>
+    </div>
+  );
 };
 
 export default Toggle;
